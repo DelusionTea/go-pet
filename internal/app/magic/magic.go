f8d189f5ae21ffd100f5d9adb061169ddf61961b
DelusionTea <DelusionKind@gmail.com>
2022-05-10 17:04:53 +0300

    hardcode magicNum

--- internal/app/magic/magic.go
+++ internal/app/magic/magic.go
@@ -2,16 +2,16 @@ package magic
 
 import (
 	"log"
-	"time"
 )
 
 //Вызывается, после успешной регистрации
 func Magic(order []byte) (float64, error) {
-	log.Println("Call Magic func - it's pretty. Order is: ", order)
-	//Сделать магию
-	var magicSolt = 5.4
-	time.Sleep(1 * time.Second)
-	magicNum := magicSolt + float64(len(order))
+	//log.Println("Call Magic func - it's pretty. Order is: ", order)
+	////Сделать магию
+	//var magicSolt = 5.4
+	//time.Sleep(1 * time.Second)
+	//magicNum := magicSolt + float64(len(order))
+	magicNum := 30.5
 	log.Println("End ofMagic func - it's pretty. Num is: ", magicNum)
 	return magicNum, nil
 }
